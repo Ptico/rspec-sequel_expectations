15c4fd9f53d692085e55cf0d083452818734f47c
Yevhenii Kurtov <lessless@aejis.eu>
2014-11-03 15:26:34 +0200

    added #with_types helper

--- lib/rspec/sequel_expectations/matchers/have_enum.rb
+++ lib/rspec/sequel_expectations/matchers/have_enum.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 module RSpec
   module Matchers
     module Sequel
@@ -5,23 +7,54 @@ module RSpec
       # http://www.rubydoc.info/gems/sequel/4.13.0/Sequel/Postgres/EnumDatabaseMethods
       class HaveEnum
         def matches?(db)
-            x = db.fetch("SELECT '#{@enum_name}'::regtype;").first
-            x[:regtype] == @enum_name
+           @db = db
+           enum_exists? &&  with_valid_types?
+        end
+        
+        def failure_message_when_negated
+          "expected database not to #{@description} #{@error}"
+        end
+        
+        def failure_message
+          "expected database to #{description} #{@error}"
+        end
+        
+        def with_types(types)
+          raise ArgumentError, 'Types must be an array' unless types.is_a? Array 
+          @enum_types = types
+          self
+        end
+
+        private
+
+        def description
+          text = [%(have enum named "#{@enum_name}")]
+          text << %(with types "#{@enum_types.join(', ')}") unless @enum_types.empty?
+          text.join(' ')
+        end
+
+        def enum_exists?
+            query = @db.fetch("SELECT '#{@enum_name}'::regtype;").first
+            query[:regtype] == @enum_name
           rescue ::Sequel::DatabaseError => e
             return false if e.message[0..18] == 'PG::UndefinedObject'
             raise e
         end
-        
-        def failure_message_when_negated
-          "expected database not to have enum '#{@enum_name}'"
+
+        def with_valid_types?
+          return true if @enum_types.empty?
+          sql = "SELECT e.enumlabel FROM pg_enum e JOIN pg_type t ON t.oid = e.enumtypid WHERE t.typname = '#{@enum_name}';"
+          types = @db.fetch(sql).reduce([]) { |memo, enum| memo << enum[:enumlabel]}
+          if @enum_types == types
+            true
+          else
+            @error = "but got #{types}"
+            false
+          end
         end
-        
-        def failure_message
-          "expected database to have enum '#{@enum_name}'"
-        end
-        
-        private
+
         def initialize(enum_name)
+          @enum_types = []
           @enum_name = enum_name
         end
       end
--- spec/matchers/have_enum_spec.rb
+++ spec/matchers/have_enum_spec.rb
@@ -3,20 +3,35 @@ require 'spec_helper'
 describe RSpec::Matchers::Sequel::HaveEnum do
   before :all do
     @enum_name = Faker::Lorem.word
-    DB.create_enum(@enum_name, %w(admin manager user inspector))
+    @types = %w(admin manager user inspector)
+    DB.create_enum(@enum_name, @types)
   end
 
   after :all do
     DB.drop_enum(@enum_name)
   end
 
-  describe '#validate_existence_of_enum' do 
+  describe '#have_enum' do 
     it 'return true if enum exists' do 
       expect(DB).to have_enum(@enum_name)
     end
 
     it 'return false if enum does not exists' do 
       expect(DB).to_not have_enum(@enum_name + "1")
+    end
+  end
+
+  describe '#with_types' do 
+    it 'accept array of fields as argument' do
+      expect{described_class.new.with_types(1)}.to raise_error(ArgumentError) 
+    end
+
+    it 'returns true if enum have exactly that values' do
+      expect(DB).to have_enum(@enum_name).with_types(@types) 
+    end
+
+    it 'returns false if enum values differ' do
+      expect(DB).not_to have_enum(@enum_name).with_types(@types.push "something_new" ) 
     end
   end
 end
